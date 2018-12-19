437160fc4c280661f2ad6c0337d2778b6662dacd
AccentureHavey <15800908003@163.com>
2018-12-19 14:00:34 +0800

    commit

---
+++ JvEmsV8/JvEmsV8Web/Controllers/DevTestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JvEmsV8Web.Controllers
+{
+    public class DevTestController : Controller
+    {
+        //
+        // GET: /DevTest/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
